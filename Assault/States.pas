6da72212c1b5619a1b9e974246e1dfb545785806
VgaSoft <VgaSoft@64132b5e-271e-0410-b17e-3f8db657d094>
2006-09-24 19:31:35 +0000

    Assault: missing file States.pas added

---
+++ Assault/States.pas
@@ -0,0 +1,25 @@
+unit States;
+
+interface
+
+uses
+  UGame, ULog, GameStates, StateIntro, StateMenu, StateGame, StateLoad, StateConsole;
+
+procedure InitStates;
+
+implementation
+
+procedure InitStates;
+var
+  Intro: Cardinal;
+begin
+  Log('Init states');
+  Intro:=Game.AddState(TStateIntro.Create);
+  Game.AddState(TStateMenu.Create);
+  Game.AddState(TStateLoad.Create);
+  Game.AddState(TStateGame.Create);
+  Game.AddState(TStateConsole.Create);
+  Game.SwitchState(Intro);
+end;
+
+end.
