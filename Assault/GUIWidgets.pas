75e18aca347e3c817400d8feb6912ccd5ccab9f3
VgaSoft <VgaSoft@64132b5e-271e-0410-b17e-3f8db657d094>
2006-12-11 10:36:15 +0000

    Assault: * Added missing file GUIWidgets.pas

---
+++ Assault/GUIWidgets.pas
@@ -0,0 +1,39 @@
+unit GUIWidgets;
+
+interface
+
+uses
+  Windows, AvL, avlUtils, dglOpenGL, OpenGLExt, UGUI;
+
+type
+  TOnButtonClick=procedure(Sender: TObject; Button: Integer) of object;
+  TGUIButtonBase=class(TGUIWidget)
+  private
+    FOnClick: TOnEvent;
+    FOnButtonClick: TOnButtonClick;
+  protected
+    procedure EventHandler(Event: TRegionEvent; Button, X, Y, Tag: Integer); override;
+  public
+    property OnClick: TOnEvent read FOnClick write FOnClick;
+    property OnButtonClick: TOnButtonClick read FOnButtonClick write FOnButtonClick;
+  end;
+
+implementation
+
+procedure TGUIButtonBase.EventHandler(Event: TRegionEvent; Button, X, Y, Tag: Integer);
+begin
+  inherited EventHandler(Event, Button, X, Y, Tag);
+  FParentGUI.GrabFocus(Self);
+  if (Event=reKeyDown) and ((Button=VK_SPACE) or (Button=VK_RETURN)) then
+  begin
+    Event:=reMouseClick;
+    Button:=1;
+  end;
+  if Event=reMouseClick then
+  begin
+    if (Button=1) and Assigned(FOnClick) then FOnClick(Self);
+    if Assigned(FOnButtonClick) then FOnButtonClick(Self, Button);
+  end;
+end;
+
+end.
