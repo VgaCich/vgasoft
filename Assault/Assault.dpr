008dd8d71df4bb177f98a98b05131bddef763193
VgaSoft <VgaSoft@64132b5e-271e-0410-b17e-3f8db657d094>
2006-12-15 09:15:16 +0000

    Assault: system units replacements by Avenger no longer used.

--- Assault/Assault.dpr
+++ Assault/Assault.dpr
@@ -3,12 +3,11 @@ program Assault;
 {$R *.res}
 
 uses
-  SysSfIni,{ FastMM4,} VSEConfig, VSEMain, States;
+  {FastMM4,} VSEConfig, VSEMain, States;
 
 begin
   VSEConfig.InitStates:=States.InitStates;
   Caption:='Assault';
   Version:='0.1';
-  CaptionVer:=Caption+' '+Version;
   VSEStart;
 end.
