057d37ca52d35b58f5101604923be27fa0ab6bfe
VgaSoft <VgaSoft@64132b5e-271e-0410-b17e-3f8db657d094>
2007-01-04 19:20:04 +0000

    Added 3rd party library J2000Dec sources (libJ2000 with stripped encoding part)

---
+++ C Libraries/J2000Dec/J2000_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef J2000_PRIVATE_H
+#define J2000_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.9.0"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	9
+#define VER_BUILD	0
+#define COMPANY_NAME	""
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"J2000 JPEG2000 decoding library"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Copyright (c) 2001-2002, David Janssens; Copyright (c) 2004-2006 VgaSoft"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	"J2000Dec.dll"
+#define PRODUCT_NAME	"J2000"
+#define PRODUCT_VERSION	""
+
+#endif /*J2000_PRIVATE_H*/
