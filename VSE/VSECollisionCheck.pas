7bfdcfae673be0073e8dcb7c345b0d3abe0a5e65
VgaSoft <VgaSoft@64132b5e-271e-0410-b17e-3f8db657d094>
2008-08-26 21:32:11 +0000

    VSE * Fullscreen entering fix * PointInCircle function * Memory count logging fixed

--- VSE/VSECollisionCheck.pas
+++ VSE/VSECollisionCheck.pas
@@ -6,6 +6,7 @@ uses
   Windows;
 
 function PointInRect(Point: TPoint; Rect: TRect): Boolean;
+function PointInCircle(Point: TPoint; Center: TPoint; Radius: Integer): Boolean;
 
 implementation
 
@@ -13,6 +14,11 @@ function PointInRect(Point: TPoint; Rect: TRect): Boolean;
 begin
   Result:=(Point.X>=Rect.Left) and (Point.X<=Rect.Right) and
           (Point.Y>=Rect.Top) and (Point.Y<=Rect.Bottom);
+end;
+
+function PointInCircle(Point: TPoint; Center: TPoint; Radius: Integer): Boolean;
+begin
+  Result:=Sqr(Center.X-Point.X)+Sqr(Center.Y-Point.Y)<=Sqr(Radius);
 end;
 
 end.
